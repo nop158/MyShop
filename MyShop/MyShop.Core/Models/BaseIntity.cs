8b0e4a1c978643f25fbcc61232ea3fa75fca312a
manop <manop@m2solution.co.th>
2018-08-21 11:50:52 +0700

    Implimented generic repository

---
+++ MyShop/MyShop.Core/Models/BaseIntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public abstract class BaseIntity
+    {
+        public string Id { get; set; }
+        public DateTimeOffset CreateAt { get; set; }
+
+        public BaseIntity()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.CreateAt = DateTime.Now;
+        }
+    }
+}
--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -8,9 +8,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MyShop.Core.Models
 {
-    public class Product
+    public class Product : BaseIntity
     {
-        public string Id { get; set; }
+
 
         [StringLength(20)]
         [DisplayName("Product Name")]
@@ -21,10 +21,10 @@ namespace MyShop.Core.Models
         public string Category { get; set; }
         public string Image { get; set; }
 
-        public Product()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
+        //public Product()
+        //{
+        //    this.Id = Guid.NewGuid().ToString();
+        //}
 
     }
 

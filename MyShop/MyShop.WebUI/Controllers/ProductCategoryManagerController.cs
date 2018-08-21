d7fab59797e504433540b95979a52060c8f1b6f7
manop <manop@m2solution.co.th>
2018-08-21 14:06:39 +0700

    Dependency Unity

--- MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contract;
+using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,11 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductCategoryManagerController : Controller
     {
-        InMemoryRepository<ProductCategory> context;
+        IRepository<ProductCategory> context;
 
-        public ProductCategoryManagerController()
+        public ProductCategoryManagerController(IRepository<ProductCategory> contexts)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            context = contexts;
         }
         // GET: ProductManager
         public ActionResult Index()
